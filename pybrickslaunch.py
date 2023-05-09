8c4cb65603494a490bb92907ae987128b890de74
johnmeshulam <johnmeshulam@gmail.com>
2023-05-09 15:28:00 +0300

    error handling

--- pybrickslaunch.py
+++ pybrickslaunch.py
@@ -5,4 +5,9 @@ import subprocess
 
 target = os.getenv("TARGET")
 command = f"pybricksdev run ble --name {HUB_NAME} {target}"
-subprocess.run(command, shell=True, check=True)
+
+try:
+    subprocess.run(command, shell=True, check=True)
+except subprocess.CalledProcessError:
+    print("Error uploading code to hub")
+    print("Make sure to set HUB_NAME and turn the hub on")
