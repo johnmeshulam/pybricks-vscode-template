ad5e331134e9d5ce43aab8e79e59492011b5c079
irrit-im <meyerovitch.idit@gmail.com>
2024-12-30 14:53:46 +0200

    very ugly code but it works

--- port_map.py
+++ port_map.py
@@ -4,31 +4,33 @@ from pybricks.pupdevices import ColorSensor, Motor, ForceSensor, UltrasonicSenso
 from pybricks.tools import wait
 from pybricks.parameters import Button
 
+PORTS = (Port.A, Port.B, Port.C, Port.D, Port.E, Port.F)
+PORT_PIXELS = {
+    Port.A: (0, 0),
+    Port.B: (0, 4),
+    Port.C: (2, 0),
+    Port.D: (2, 4),
+    Port.E: (4, 0),
+    Port.F: (4, 4),
+}
+
 hub = PrimeHub()
 
 
-def map_ports() -> dict[Port, (ColorSensor | Motor | ForceSensor | UltrasonicSensor)]:
+def map_ports() -> list[ColorSensor | Motor | ForceSensor | UltrasonicSensor]:
     available_device_types = (ColorSensor, Motor, ForceSensor, UltrasonicSensor)
 
-    ports = (Port.A, Port.B, Port.C, Port.D, Port.E, Port.F)
-    port_devices = {
-        Port.A: None,
-        Port.B: None,
-        Port.C: None,
-        Port.D: None,
-        Port.E: None,
-        Port.F: None,
-    }
+    devices = []
 
-    for port in ports:
+    for port in PORTS:
         for device_type in available_device_types:
             try:
-                port_devices[port] = device_type(port)
+                devices.append(device_type(port))
                 break
             except OSError:
                 ...
 
-    return port_devices
+    return devices
 
 
 def get_device_name(
@@ -38,69 +40,74 @@ def get_device_name(
     return str(device_type).replace("<class '", "").replace("'>", "")
 
 
-def generate_code_difinition(device, port) -> str:
+def generate_code_difinition(
+    device: ColorSensor | Motor | ForceSensor | UltrasonicSensor, port: Port
+) -> str:
     device_name: str = get_device_name(device)
     print(f"{device_name.lower()} = {device_name}({port})")
 
 
-def print_hardware_difinitions(port_devices: dict) -> None:
-    for port, device in port_devices.items():
+def print_hardware_difinitions(
+    devices: list[ColorSensor | Motor | ForceSensor | UltrasonicSensor],
+) -> None:
+    for i in range(6):
+        port = PORTS[i]
+        device = devices[i]
         device_name: str = get_device_name(device)
         print(f"{device_name.lower()} = {device_name}({port})")
 
 
-def signal_color_sensor(sensor: ColorSensor) -> None:
-    sensor.lights.off()
-    wait(150)
-    sensor.lights.on(100)
-    wait(150)
-
-
-def signal_motor(motor: Motor) -> None:
-    motor.run_time(100, 100, wait=False)
+def indicate_device(
+    device: ColorSensor | Motor | ForceSensor | UltrasonicSensor,
+) -> None:
+    if type(device) == ColorSensor:
+        device.lights.on(100)
+    elif type(device) == Motor:
+        device.dc(100)
 
 
 def indicate_port_on_matrix(port: Port) -> None:
-    port_pixels = {
-        Port.A: (0, 0),
-        Port.B: (0, 4),
-        Port.C: (2, 0),
-        Port.D: (2, 4),
-        Port.E: (4, 0),
-        Port.F: (4, 4),
-    }
     hub.display.off()
-    x, y = port_pixels[port]
+    x, y = PORT_PIXELS[port]
     hub.display.pixel(x, y)
 
 
-def signal_devices(port_devices: dict) -> None:
-    indicator = 0  # TODO: rename
+def stop_device(device: ColorSensor | Motor | ForceSensor | UltrasonicSensor) -> None:
+    if type(device) == ColorSensor:
+        device.lights.off()
+    elif type(device) == Motor:
+        device.stop()
+
+
+def signal_devices(
+    devices: list[ColorSensor | Motor | ForceSensor | UltrasonicSensor],
+) -> None:
+    indicator = 0
 
     while True:
+        running_device = devices[indicator]
+        indicate_port_on_matrix(port=PORTS[indicator])
+        indicate_device(running_device)
+
         if Button.RIGHT in hub.buttons.pressed():
-            indocator = (indicator + 1) % 6
+            stop_device(running_device)
+            indicator = (indicator + 1) % 6
+            wait(200)
         if Button.LEFT in hub.buttons.pressed():
-            indocator = (indicator - 1) % 6
+            stop_device(running_device)
+            indicator = (indicator - 1) % 6
+            wait(200)
 
-        port, device = port_devices.items()[indicator]
-        indicate_port_on_matrix(port=port)
-        device_name: str = get_device_name(device)
-        if device_name == "ColorSensor":
-            signal_color_sensor(device)
-        elif device_name == "Motor":
-            signal_motor(device)
-        else:
-            wait(100)
+        wait(20)
 
 
-device_ports = map_ports()
-print_hardware_difinitions(device_ports)
-signal_devices(device_ports)
-print("done")
-# <class 'ColorSensor'> = <class 'ColorSensor'>(Port.C)
-# <class 'Motor'> = <class 'Motor'>(Port.F)
-# <class 'Motor'> = <class 'Motor'>(Port.A)
-# <class 'ColorSensor'> = <class 'ColorSensor'>(Port.D)
-# <class 'Motor'> = <class 'Motor'>(Port.B)
-# <class 'Motor'> = <class 'Motor'>(Port.E)
+def main() -> None:
+    devices = map_ports()
+    print_hardware_difinitions(devices)
+    for device in devices:
+        if type(device) == ColorSensor:
+            device.lights.off()
+    signal_devices(devices)
+
+
+main()
